cfa1f79dd1582a182ed42129f34e490c54ad74ee
skylarmt <apocalypselabs@gmail.com>
2015-02-27 14:19:36 -0700

    Relicense Debug class to public domain, add javadocs to it.

--- src/net/apocalypselabs/symat/Debug.java
+++ src/net/apocalypselabs/symat/Debug.java
@@ -1,47 +1,11 @@
 /* 
- * CODE LICENSE =====================
- * Copyright (c) 2015, Apocalypse Laboratories
- * All rights reserved.
+ * This class is public domain because it is standalone and 
+ * is potentially useful to other projects.
  * 
- * Redistribution and use in source and binary forms, with or without modification,
- *  are permitted provided that the following conditions are met:
+ * If this cannot be placed in the public domain, it is licensed under
+ * the Creative Commons Zero license.
  * 
- * 1. Redistributions of source code must retain the above copyright notice, this 
- * list of conditions and the following disclaimer.
- * 
- * 2. Redistributions in binary form must reproduce the above copyright notice, 
- * this list of conditions and the following disclaimer in the documentation and/or
- *  other materials provided with the distribution.
- * 
- * 3. Neither the name of the copyright holder nor the names of its contributors 
- * may be used to endorse or promote products derived from this software without 
- * specific prior written permission.
- * 
- * 4. You adhere to the Media License detailed below.  If you do not, this license
- * is automatically revoked and you must purge all copies of the software you
- * possess, in source or binary form.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
- *  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON 
- * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
- * MEDIA LICENSE ====================
- * All images and other graphical files (the "graphics") included with this
- * software are copyright (c) 2015 Apocalypse Laboratories.  You may not distribute
- * the graphics or any program, source code repository, or other digital storage 
- * media containing them without written permission from Apocalypse Laboratories.
- * This ban on distribution only applies to publicly available systems.
- * A password-protected network file share, USB drive, or other storage scheme that
- * cannot be easily accessed by the public is generally allowed.  If in doubt,
- * contact Apocalypse Laboratories.  If Apocalypse Laboratories allows or denies
- * you permission, that decision is considered final and binding.
+ * Feel free to use it however you wish!
  */
 package net.apocalypselabs.symat;
 
@@ -53,23 +17,51 @@ package net.apocalypselabs.symat;
 public class Debug {
 
     // If output should be on or off
-    public static final boolean DEBUG = true;
+    public static boolean debug = false;
+    
+    /**
+     * Turn debug output on and off.
+     * @param b 
+     */
+    public static void setDebug(boolean b) {
+        debug = b;
+    }
+    
+    /**
+     * Check if debug output is enabled.
+     * @return true if it is.
+     */
+    public static boolean getDebug() {
+        return debug;
+    }
 
+    /**
+     * Call System.out.println(data) if debug output enabled.
+     * @param data Data to print.
+     */
     public static void println(Object data) {
-        if (DEBUG) {
+        if (debug) {
             System.out.println(data);
         }
     }
 
+    /**
+     * Call System.err.println(data) if debug output enabled.
+     * @param data Data to print.
+     */
     public static void printerr(Object data) {
-        if (DEBUG) {
+        if (debug) {
             System.err.println(data);
         }
     }
 
-    @SuppressWarnings(value = {"CallToPrintStackTrace"}) 
+    /**
+     * Call e.printStackTrace() if debug output enabled.
+     * @param e an Exception.
+     */
+    @SuppressWarnings(value = {"CallToPrintStackTrace"})
     public static void stacktrace(Exception e) {
-        if (DEBUG) {
+        if (debug) {
             e.printStackTrace();
         }
     }
